04462307210bd290008455914dc4dc4659d4914f
GirmaMogesTekle <girma.moges.teklemariam@outlook.com>
2025-12-23 11:58:40 -0600

    Add Base Entity for Auditing

---
+++ src/main/java/com/sample/projects/postandcomments/entity/BaseEntity.java
@@ -0,0 +1,36 @@
+package com.sample.projects.postandcomments.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@SuperBuilder
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @CreatedDate
+//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name ="created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+//    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}

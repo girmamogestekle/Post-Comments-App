c2da5d2581412200f465ee121d30f961d534edc4
GirmaMogesTekle <girma.moges.teklemariam@outlook.com>
2025-12-23 18:24:51 -0600

    Fix PostControllerTest and MainTest Classes

--- src/test/java/com/sample/projects/postandcomments/PostAndCommentsApplicationTests.java
+++ src/test/java/com/sample/projects/postandcomments/PostAndCommentsApplicationTests.java
@@ -1,9 +1,14 @@
 package com.sample.projects.postandcomments;
 
+import com.sample.projects.postandcomments.config.TestAiConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestAiConfig.class)
 class PostAndCommentsApplicationTests {
 
     @Test
